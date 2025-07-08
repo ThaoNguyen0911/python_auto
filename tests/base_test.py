0b2fa0ba010fbff382a5e4c0c73b21d23574cfac
ThaoNguyen0911 <ttthaonguyen0911@gmail.com>
2025-07-08 20:57:51 +0700

    fix 4

--- tests/base_test.py
+++ tests/base_test.py
@@ -18,7 +18,7 @@ class BaseTest:
         request.cls.session = session
         options = webdriver.ChromeOptions()
         # options.add_argument("--headless")
-        # options.add_argument("--no-sandbox")
+        options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         # Initialize the Chrome WebDriver instance
         self.driver = webdriver.Chrome(options=options)
